63dd49c9b1121a230bf2343bc9af2a4e7d1a277a
unknown <sanchit2005yudavanshi@gmail.com>
2023-11-30 20:27:03 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import BookContextProvider from "./context/BookContext";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import BookContextProvider from './context/BookContext';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BookContextProvider>
-      <App />
+    <App />
     </BookContextProvider>
   </React.StrictMode>
 );
