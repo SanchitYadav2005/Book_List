97fcc2cda807f5d662df02f5c86b031594ed4341
unknown <sanchit2005yudavanshi@gmail.com>
2023-12-01 19:41:14 +0530

    added all the components to the root and added some styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import BookForm from './components/BookForm';
+import BookList from './components/BookList';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <BookList/>
+      <BookForm/>
     </div>
   );
 }
